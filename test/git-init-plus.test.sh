782f6d0bdd61b00b6583f84c7339ec93bf3018a8
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-16 22:00:48 +0000

    Test git folder is initialized

--- test/git-init-plus.test.sh
+++ test/git-init-plus.test.sh
@@ -1,6 +1,19 @@
 #! /bin/sh
 # file: examples/equality_test.sh
 
+testGitIsInitialized()
+{
+
+    mkdir temp-test-dir
+    cd temp-test-dir  || exit
+    ../git-init-plus.sh
+    exists=false
+    if test -f ".git/hooks/commit-msg.sample"; then exists=true;fi
+    assertEquals true "$exists"
+    cd ..
+    rm -rf temp-test-dir
+}
+
 testLicenseIsCreated()
 {
 
