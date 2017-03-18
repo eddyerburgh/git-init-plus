fd048cdbb17bc4e622dc1b2e246005574b555a62
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-18 09:56:40 +0000

    Add shellcheck to test script

--- test.sh
+++ test.sh
@@ -8,3 +8,9 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 shunit2 "$SCRIPT_PATH/test/license.test.sh"
 shunit2 "$SCRIPT_PATH/test/readme.test.sh"
 shunit2 "$SCRIPT_PATH/test/git.test.sh"
+
+shellcheck "$SCRIPT_PATH/test/license.test.sh"
+shellcheck "$SCRIPT_PATH/test/readme.test.sh"
+shellcheck "$SCRIPT_PATH/test/git.test.sh"
+
+shellcheck "$SCRIPT_PATH/git-init-plus.sh"
