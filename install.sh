f32b82016478b7c4e3f072ccb2fc4f1d13636f20
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-19 16:31:28 +0000

    Prompt user for permission before replacing files required for install

--- install.sh
+++ install.sh
@@ -11,13 +11,48 @@ fatal()   { echo "[FATAL]   $@" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
 
 wget https://github.com/eddyerburgh/git-init-plus/archive/master.zip
 
-info "Downloading zip from github"
+# Prompt for permission to remove any existing files/folders that are used in install
+if [ -e "/opt/git-init-plus-master.zip" ]; then
+  read -r -p "/opt/git-init-plus-master.zip already exists, OK to replace? [y/N] " response
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      rm /opt/git-init-plus-master.zip
+      mv master.zip /opt/git-init-plus-master.zip
+      ;;
+    *)
+      info "Install exiting. Please rename /opt/git-init-plus-master.zip and rerun install script"
+      exit 2
+    ;;
+  esac
+else
+  mv master.zip /opt/git-init-plus-master.zip
+fi
 
-[ -e /opt/git-init-master.zip ] && rm /opt/git-init-master.zip
-[ -e /opt/git-init-plus ] && rm -rf /opt/git-init-plus
-[ -e /opt/git-init-plus-master ] && rm -rf /opt/git-init-plus-master
+if [ -e "/opt/git-init-plus-master" ]; then
+  read -r -p "/opt/git-init-plus-master directory already exists, OK to replace? [y/N] " response
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      rm -rf "/opt/git-init-plus-master"
+      ;;
+    *)
+      info "Install exiting. Please rename /opt/git-init-plus-master and rerun install script"
+      exit 2
+    ;;
+  esac
+fi
 
-mv master.zip /opt/git-init-plus-master.zip
+if [ -e "/opt/git-init-plus" ]; then
+  read -r -p "/opt/git-init-plus directory already exists, OK to replace? [y/N] " response
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      rm -rf "/opt/git-init-plus"
+      ;;
+    *)
+      info "Install exiting. Please rename /opt/git-init-plus and rerun install script"
+      exit 2
+    ;;
+  esac
+fi
 
 cd /opt
 unzip git-init-plus-master.zip
@@ -32,5 +67,5 @@ ln -sf "$ROOT_DIR/git-init-plus.sh" /usr/local/bin/git-init-plus
 
 info "Success! git-init-plus is installed in /usr/local/bin
 Run git-init-plus to start a new git project
-If git-init-plus is no recognised as a command, add /usr/loca/bin to your PATH
+If git-init-plus is no recognised as a command, add /usr/local/bin to your PATH
 "
--- test/install.test.sh
+++ test/install.test.sh
@@ -2,16 +2,71 @@
 
 ROOT_PATH=$(pwd -P)
 
-test_it_adds_git_init_plus_to_path()
+setUp()
 {
-  [ -e "/usr/local/bin/git-init-plus" ] && rm "/usr/local/bin/git-init-plus"
+  [ -e "~/Downloads/master.zip" ] && rm "~/Downloads/master.zip"
+  [ -e "/opt/git-init-plus-master.zip" ] && rm "/opt/git-init-plus-master.zip"
+  [ -e "/opt/git-init-plus-master" ] && rm -rf "/opt/git-init-plus-master"
+  [ -e "/opt/git-init-plus" ] && rm -rf "/opt/git-init-plus"
+  [ -e "test-dir" ] && rm -rf "test-dir"
   mkdir test-dir
   cd test-dir
+}
+
+tearDown()
+{
+    cd ..
+    rm -rf test-dir
+}
+
+test_it_adds_git_init_plus_to_path()
+{
   "$ROOT_PATH/install.sh"
   binary_exists_in_path=false
   if hash git-init-plus 2>/dev/null; then binary_exists_in_path=true;fi
   assertTrue "$binary_exists_in_path"
+}
 
-  cd ..
-  rm -rf test-dir
+test_it_requires_prompt_to_delete_git_init_zip_in_opt()
+{
+  ZIP_FILE="/opt/git-init-plus-master.zip"
+  touch "$ZIP_FILE"
+  echo "Content" > "$ZIP_FILE"
+  mkdir test-dir
+  cd test-dir
+  printf "n" | "$ROOT_PATH/install.sh"
+
+  zip_not_replaced=false
+  if grep -q "Content" "$ZIP_FILE"; then zip_not_replaced=true;fi
+  assertEquals true "$zip_not_replaced"
+}
+
+test_it_requires_prompt_to_delete_git_init_master_dir_in_opt()
+{
+  GIT_INIT_MASTER_DIR="/opt/git-init-plus-master"
+  mkdir "$GIT_INIT_MASTER_DIR"
+  touch "$GIT_INIT_MASTER_DIR/example"
+  echo "Content" > "$GIT_INIT_MASTER_DIR/example"
+  mkdir test-dir
+  cd test-dir
+  printf "n" | "$ROOT_PATH/install.sh"
+
+  file_not_replaced=false
+  if grep -q "Content" "$GIT_INIT_MASTER_DIR/example"; then file_not_replaced=true;fi
+  assertEquals true "$file_not_replaced"
+}
+
+test_it_requires_prompt_to_delete_git_init_dir_in_opt()
+{
+  GIT_INIT_DIR="/opt/git-init-plus"
+  mkdir "$GIT_INIT_DIR"
+  touch "$GIT_INIT_DIR/example"
+  echo "Content" > "$GIT_INIT_DIR/example"
+  mkdir test-dir
+  cd test-dir
+  printf "n" | "$ROOT_PATH/install.sh"
+
+  file_not_replaced=false
+  if grep -q "Content" "$GIT_INIT_DIR/example"; then file_not_replaced=true;fi
+  assertEquals true "$file_not_replaced"
 }
