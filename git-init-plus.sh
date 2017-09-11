faf24b509b1cb66147c64737a8893b822f46209e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-11 15:47:48 +0100

    refactor: format git-init-plus

--- git-init-plus.sh
+++ git-init-plus.sh
@@ -5,8 +5,8 @@ shopt -s expand_aliases
 
 function patch_readlink() {
 	[[ $(uname) == 'Darwin' ]] && {
-		which greadlink gsed > /dev/null && {
-	      unalias readlink sed
+		which greadlink gsed >/dev/null && {
+			unalias readlink sed
 			alias readlink=greadlink sed=gsed
 		} || {
 			echo 'ERROR: GNU utils required for Mac. You may use homebrew to install them: brew install coreutils gnu-sed'
@@ -27,16 +27,19 @@ patch_readlink
 #/   -h: Display this help message
 #/   --help: Display this help message
 usage() {
-    grep '^#/' "$0" | cut -c4-
-    exit 0
+	grep '^#/' "$0" | cut -c4-
+	exit 0
 }
-expr "$*" : ".*--help" > /dev/null && usage
-expr "$*" : ".*-h" > /dev/null && usage
+expr "$*" : ".*--help" >/dev/null && usage
+expr "$*" : ".*-h" >/dev/null && usage
 
 # Logger
 readonly LOG_FILE="/tmp/git-init-plus.log"
-info()    { echo "$@" | tee -a "$LOG_FILE" >&2 ; }
-fatal()   { echo "$@" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+info() { echo "$@" | tee -a "$LOG_FILE" >&2; }
+fatal() {
+	echo "$@" | tee -a "$LOG_FILE" >&2
+	exit 1
+}
 
 command -v git >/dev/null 2>&1 || { fatal "git-init-plus requires git but it's not installed.  Aborting."; }
 
@@ -49,31 +52,30 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 license=
 name=
 project_name=
-while getopts l:n:p: option
-do
-  case "${option}"
-  in
-    l) license=${OPTARG};;
-    n) name=${OPTARG};;
-    p) project_name=${OPTARG};;
-  esac
+while getopts l:n:p: option; do
+	case "${option}" in
+
+	l) license=${OPTARG} ;;
+	n) name=${OPTARG} ;;
+	p) project_name=${OPTARG} ;;
+	esac
 done
 
 initialize_git_repo() {
-# Initialize empty git repo
-if [ -d "$WORKING_PATH/.git" ]; then
-  read -r -p ".git already exists in directory, do you want to reinitialize? [y/N] " response
-  case "$response" in
-    [yY][eE][sS]|[yY])
-      git init
-      ;;
-    *)
-      fatal "Program exiting"
-    ;;
-  esac
-else
-  git init
-fi
+	# Initialize empty git repo
+	if [ -d "$WORKING_PATH/.git" ]; then
+		read -r -p ".git already exists in directory, do you want to reinitialize? [y/N] " response
+		case "$response" in
+		[yY][eE][sS] | [yY])
+			git init
+			;;
+		*)
+			fatal "Program exiting"
+			;;
+		esac
+	else
+		git init
+	fi
 }
 
 create_license() {
@@ -81,36 +83,35 @@ create_license() {
 	LICENSE="$WORKING_PATH/LICENSE"
 
 	if [ "$license" ]; then
-	  license_reference_file="$SCRIPT_PATH/resources/licenses/$license.txt"
-	  if [ ! -e "$license_reference_file" ]; then
-	    licenses_list=""
-	    for file in $SCRIPT_PATH/resources/licenses/*.txt; do
-	      if [ ! "$licenses_list" ]; then
-	        licenses_list=$(basename "${file%.*}")
-	      else
-	        licenses_list="$licenses_list, $(basename "${file%.*}")"
-	      fi
-	    done
-	    fatal "Invalid license passed to function. Available licenses are ${licenses_list}"
-	  fi
-	  cat "$license_reference_file" >> "$LICENSE"
-	  info "Created $license license"
+		license_reference_file="$SCRIPT_PATH/resources/licenses/$license.txt"
+		if [ ! -e "$license_reference_file" ]; then
+			licenses_list=""
+			for file in $SCRIPT_PATH/resources/licenses/*.txt; do
+				if [ ! "$licenses_list" ]; then
+					licenses_list=$(basename "${file%.*}")
+				else
+					licenses_list="$licenses_list, $(basename "${file%.*}")"
+				fi
+			done
+			fatal "Invalid license passed to function. Available licenses are ${licenses_list}"
+		fi
+		cat "$license_reference_file" >>"$LICENSE"
+		info "Created $license license"
 
 	else
-	  cat "$SCRIPT_PATH/resources/licenses/MIT.txt" >> "$LICENSE"
-	  info "No license specified, defaulting to MIT (pass license with -l arg)"
+		cat "$SCRIPT_PATH/resources/licenses/MIT.txt" >>"$LICENSE"
+		info "No license specified, defaulting to MIT (pass license with -l arg)"
 	fi
 
 	# Add name to license
 	if [ "$name" ]; then
-	  sed -i "s/<copyright holders>/$name/g" "$LICENSE"
+		sed -i "s/<copyright holders>/$name/g" "$LICENSE"
 	else
-	  while [[ $name == '' ]]
-	  do
-	    read -r -p "What is the name(s) of the copyright holder(s):" name
-	  done
-	  sed -i "s/<copyright holders>/$name/g" "$LICENSE"
-	  info "Name added to license"
+		while [[ $name == '' ]]; do
+			read -r -p "What is the name(s) of the copyright holder(s):" name
+		done
+		sed -i "s/<copyright holders>/$name/g" "$LICENSE"
+		info "Name added to license"
 	fi
 
 	# Add date to license
@@ -123,14 +124,13 @@ create_license() {
 
 	# Add project name as title of README
 	if [ "$project_name" ]; then
-	  echo "# $(tr '[:upper:]' '[:lower:]' <<<"$project_name")" > "$README"
+		echo "# $(tr '[:upper:]' '[:lower:]' <<<"$project_name")" >"$README"
 	else
-	  while [[ $project_name == '' ]]
-	  do
-	    read -r -p "What is the name your project (added as title to README):" project_name
-	  done
-	  echo "# $(tr '[:upper:]' '[:lower:]' <<<"$project_name")" > "$README"
-	  info "Title added to README"
+		while [[ $project_name == '' ]]; do
+			read -r -p "What is the name your project (added as title to README):" project_name
+		done
+		echo "# $(tr '[:upper:]' '[:lower:]' <<<"$project_name")" >"$README"
+		info "Title added to README"
 	fi
 
 }
@@ -149,5 +149,3 @@ git_init_plus() {
 	create_gitignore
 	info "Success! New project initialized"
 }
-
-git_init_plus
