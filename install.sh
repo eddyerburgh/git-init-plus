bd13d4fbadedcf9c8cdd29e5b1c5ad6291569cb3
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-14 08:16:46 +0100

    refactor: use temp directory in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -uo pipefail
-IFS=$'\n\t'
 
-# Logger
+IFS=$'\n\t'
+SCRIPT_NAME="git-init-plus"
+TMP_DIR="/tmp/${SCRIPT_NAME}.$RANDOM.$RANDOM.$RANDOM.$$"
+(umask 077 && mkdir "${TMP_DIR}") || {
+	die "Could not create temporary directory! Exiting."
+}
+
 readonly LOG_FILE="/tmp/$(basename "$0").log"
 info() { echo "$@" | tee -a "$LOG_FILE" >&2; }
 fatal() {
@@ -16,76 +21,41 @@ gip_check_dependencies_installed() {
 }
 
 gip_download_zip() {
-	curl -LOk https://github.com/eddyerburgh/git-init-plus/archive/master.zip
-}
-
-gip_remove_existing_files() {
-	# Prompt for permission to remove any existing files/folders that are used in install
-	if [ -e "/opt/git-init-plus-master.zip" ]; then
-		read -r -p "/opt/git-init-plus-master.zip already exists, OK to replace? [y/N] " response
-		case "$response" in
-		[yY][eE][sS] | [yY])
-			rm /opt/git-init-plus-master.zip
-			mv master.zip /opt/git-init-plus-master.zip
-			;;
-		*)
-			fatal "Install exiting. Please rename /opt/git-init-plus-master.zip and rerun install script"
-			;;
-		esac
-	else
-		mv master.zip /opt/git-init-plus-master.zip
-	fi
-
-	if [ -e "/opt/git-init-plus-master" ]; then
-		read -r -p "/opt/git-init-plus-master directory already exists, OK to replace? [y/N] " response
-		case "$response" in
-		[yY][eE][sS] | [yY])
-			rm -rf "/opt/git-init-plus-master"
-			;;
-		*)
-			fatal "Install exiting. Please rename /opt/git-init-plus-master and rerun install script"
-			;;
-		esac
-	fi
-
-	if [ -e "/opt/git-init-plus" ]; then
-		read -r -p "/opt/git-init-plus directory already exists, OK to replace? [y/N] " response
-		case "$response" in
-		[yY][eE][sS] | [yY])
-			rm -rf "/opt/git-init-plus"
-			;;
-		*)
-			fatal "Install exiting. Please rename /opt/git-init-plus and rerun install script"
-			;;
-		esac
-	fi
+	cd "$TMP_DIR" || exit
+	curl -LOk "https://github.com/eddyerburgh/{$SCRIPT_NAME}/archive/master.zip"
 }
 
 gip_unzip() {
-	cd /opt || exit
-	unzip git-init-plus-master.zip
+	cd "$TMP_DIR" || exit
+	unzip master.zip
+}
 
-	mv git-init-plus-master git-init-plus
+gip_move_to_opt() {
+	mv "${TMP_DIR}/${SCRIPT_NAME}-master" "/opt/${SCRIPT_NAME}"
 }
 
 gip_create_sym_link() {
-	ROOT_DIR=/opt/git-init-plus
+	local ROOT_DIR="/opt/${SCRIPT_NAME}"
+	chmod +x "$ROOT_DIR/${SCRIPT_NAME}.sh"
+	ln -sf "$ROOT_DIR/${SCRIPT_NAME}.sh" "/usr/local/bin/${SCRIPT_NAME}"
+}
 
-	chmod +x "$ROOT_DIR/git-init-plus.sh"
-
-	ln -sf "$ROOT_DIR/git-init-plus.sh" /usr/local/bin/git-init-plus
+gip_trap_cleanup() {
+	rm -rf "${TMP_DIR}"
 }
 
 gip_install() {
 	gip_check_dependencies_installed
 	gip_download_zip
-	gip_remove_existing_files
 	gip_unzip
+	gip_move_to_opt
 	gip_create_sym_link
-	info "Success! git-init-plus is installed in /usr/local/bin
-  Run git-init-plus to start a new git project
-  If git-init-plus is no recognised as a command, add /usr/local/bin to your PATH
+	info "Success! ${SCRIPT_NAME} is installed in /usr/local/bin
+  Run ${SCRIPT_NAME} to start a new git project
+  If ${SCRIPT_NAME} is no recognised as a command, add /usr/local/bin to your PATH
   "
 }
 
 gip_install
+
+trap gip_trap_cleanup EXIT INT TERM
